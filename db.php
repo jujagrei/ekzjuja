ae88f4a1a8b3348c640e030dd7b405cab80cb919
jujagrei <rus111112@yandex.ru>
2024-05-31 16:11:53 +0400

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $link = mysqli_connect('localhost','root','','Ferganski');
-    if(!$link) {
-        die("Ошибка: " .mysqli_connect_error());
-    }
-    /*else {echo "Соединение успешно"}*/
-?>
